552a6889294fbee01bdfbb0783ecdd6b10ffefb3
stealthscripter <natnael01ayele@gmail.com>
2024-07-31 18:19:07 +0300

    change api

--- js/app.js
+++ js/app.js
@@ -7,12 +7,15 @@ const googleMaps = document.querySelector('.maps')
 const currency = document.querySelector('.currency')
 const countryFlag = document.querySelector('.flags')
 
+
+const btnContainer = document.querySelector('.btncontainer')
 const mainContainer = document.querySelector('.hero')
-const btn = document.querySelector('.btn')
+const btnlocation = document.querySelector('.btnlocation')
+const btnip = document.querySelector('.btnip')
 const errorText = document.querySelector('.error-text')
 const initalDataContainer = document.querySelector('.inital-data')  
 const continent = document.querySelector('.continent');
-
+const btnHome = document.querySelector('.btnhome')
 
 console.log("hello World")
 
@@ -23,10 +26,58 @@ const getLocation = () => {
 }
 
 
-getLocation()
-.then(res => {
-    const {latitude , longitude } = res.coords
-})
-.catch(err => console.log(err))
 
+const handleLocation = async (locations) => {
+    try{
+        const res = await getLocation()
+        const {latitude , longitude } = res.coords
+        locations(latitude , longitude)
+    }
+    catch(err){
+      console.log(err.message) 
+    }
+}
+
+// handleLocation((latitude , longitude) => {
+//   fetch(`https://geocode.xyz/${latitude},${longitude}?geoit=json&auth=436913924601987352488x50714`)
+//   .then(res => res.json())
+//   .then(data => console.log(data))
+// })
+
+// fetch("http://ip-api.com/json/").then(res => res.json()).then(data => {const countryCode = data.countryCode; return fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`)})
+// .then(res => res.json()).then(data => console.log(data))
+
+
+const fetchIpData = async () => {
+    
+    try{
+      const res = await fetch("http://ip-api.com/json/")
+      const data = await res.json()
+      return data
+    }
+    catch(err){
+        console.log(err.message)
+    }
+
+}
+
+
+
+
+
+
+
+
+btnip.addEventListener('click',() => {
+  mainContainer.style.display = 'flex';
+  initalDataContainer.style.opacity = 0;
+  btnContainer.style.opacity = 0
+
+})
+
+btnHome.addEventListener('click', () => {
+    mainContainer.style.display = 'none'
+    initalDataContainer.style.opacity = 1
+    btnContainer.style.opacity = 1
+})
 
